94455a3854df00564a3216306ae18fa2098f2e32
benjaminshawki <benjaminshawki@gmail.com>
2023-12-18 02:26:11 +0000

    zathura background colors

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#nvim 
+# nvim 
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
@@ -8,3 +8,7 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 # X11
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+# zathura
+rm -rf "$HOME/.config/zathura"
+ln -s "$HOME/dotfiles/zathura" "$HOME/.config"
