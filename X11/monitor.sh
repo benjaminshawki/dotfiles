1edf4b4cf99e65d9f0481933f6fb952394d5dbdc
benjaminshawki <benjaminshawki@gmail.com>
2023-12-21 06:34:44 +0100

    monitor setup

--- X11/monitor.sh
+++ X11/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 2880x1800 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 2880x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1-0 --off
+xrandr --output eDP-1 --primary --mode 2880x1800 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1-0 --off
