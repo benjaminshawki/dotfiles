ddbbda99184befd5a54e94fcb838b610b40eeb89
benjaminshawki <benjaminshawki@gmail.com>
2025-04-05 14:12:23 +0200

    add backup

---
+++ X11_backup/monitor-hdmi-1080.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2880x1800 --pos 0x1080 --rotate normal --output HDMI-1-0 --mode 1920x1080 --pos 480x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1 --off
+xinput --map-to-output 'ELAN2513:00 04F3:4072' eDP-1
