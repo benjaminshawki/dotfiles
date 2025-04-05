7dbc0c928d0d234e8cebc3ea8e70d7e531f343e8
benjaminshawki <benjaminshawki@gmail.com>
2025-04-05 14:01:52 +0200

    160 fps

--- X11/monitor-usb-c-4k.sh
+++ X11/monitor-usb-c-4k.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 2880x1800 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1-0 --off
+xrandr --output eDP-1 --primary --mode 2880x1800 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1-0 --off --rate 160
 xinput --map-to-output 'ELAN2513:00 04F3:4072' eDP-1
