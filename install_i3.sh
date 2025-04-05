9e044b19a866cbdd485d9b8be4023374d59be2e6
benjaminshawki <benjaminshawki@gmail.com>
2025-04-05 14:38:42 +0200

    Experimental i3  config

--- install_i3.sh
+++ install_i3.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-########    
-# nvim # 
-########
+# nvim
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 mkdir -p "$XDG_CONFIG_HOME/undo"
 ln -sf "$DOTFILES/nvim/init.lua" "$XDG_CONFIG_HOME/nvim"
@@ -10,69 +8,55 @@ ln -sf "$DOTFILES/nvim/lua" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$DOTFILES/nvim/ftplugin/" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$DOTFILES/nvim/queries/" "$XDG_CONFIG_HOME/nvim"
 
-#######
-# X11 #
-#######
+# zathura
+rm -rf "$XDG_CONFIG_HOME/zathura"
+ln -s "$DOTFILES/zathura" "$XDG_CONFIG_HOME"
+
+# x11
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 xrdb -merge "$XDG_CONFIG_HOME/X11/.Xresources"
 
-###########
-# zathura #
-###########
-rm -rf "$XDG_CONFIG_HOME/zathura"
-ln -s "$DOTFILES/zathura" "$XDG_CONFIG_HOME"
-
-######
-# i3 #
-######
+# i3
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
-#######
-# zsh #
-#######
+# zsh
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/zsh/.zshenv_hidden" "$HOME"
 ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
-#########
-# fonts #
-#########;
+# fonts
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
-#############
-# alacritty #
-#############
+# alacritty
 rm -rf "$XDG_CONFIG_HOME/alacritty"
 ln -s "$DOTFILES/alacritty" "$XDG_CONFIG_HOME"
 
-#########
-# dunst #
-#########
+# dunst
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
-########
-# tmux #
-########
+# tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 
-[ ! -d "$XDG_CONFIG_HOME/tmux/plugins" ] \
-&& git clone https://github.com/tmux-plugins/tpm \
-"$XDG_CONFIG_HOME/tmux/plugins/tpm"
+[ ! -d "$XDG_CONFIG_HOME/tmux/plugins" ] &&
+	git clone https://github.com/tmux-plugins/tpm \
+		"$XDG_CONFIG_HOME/tmux/plugins/tpm"
 
 rm -rf "$XDG_CONFIG_HOME/tmuxp"
 ln -sf "$DOTFILES/tmuxp" "$XDG_CONFIG_HOME"
 
-##########
-# ranger #
-##########
+# ranger
 mkdir -p "$XDG_CONFIG_HOME/ranger"
 ln -sf "$DOTFILES/ranger/rc.conf" "$XDG_CONFIG_HOME/ranger/rc.conf"
+
+# wayvnc
+mkdir -p "$XDG_CONFIG_HOME/wayvnc"
+ln -sf "$DOTFILES/wayvnc/config" "$XDG_CONFIG_HOME/wayvnc/config"
